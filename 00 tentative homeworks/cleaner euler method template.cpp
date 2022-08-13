64bccc38e69c95edbcdd71c290ad32c4d940054e
Rajat Kumar Mandal <r.mandal@iitg.ac.in>
2022-08-13 18:39:54 +0530

    update

--- 00 tentative homeworks/cleaner euler method template.cpp
+++ 00 tentative homeworks/cleaner euler method template.cpp
@@ -5,17 +5,22 @@
 
 constexpr size_t dim = 2;   //dimension of the reduced 1st-order problem
 
-typedef std::array<double, dim> state_type;  //data type definition for dependant variables
+typedef std::array<double, dim> state_type;  //data type definition for dependant variables - array of x_0, x_1, ... x_n
 typedef std::vector<state_type> solution;   //data type definition for storing the list of calculated values
 
 //This is the differential Equation, reduced to first-order
 void System(const state_type& x, state_type& dxdt){
-    //fill in...
+    dxdt[0] = x[1];
+    dxdt[1] = -sin(x[0]);
 }
 
 //The stepper function, calculates x_{n+1} given the differential equation, x_{n} and step size
-void finite_diff_step(void (*System)(state_type& x, state_type& dxdt), solution& x_values, const double dt){
-    //fill in...
+void euler_step(void (*System)(const state_type& x, state_type& dxdt), state_type& x, const double dt){
+    state_type dxdt;
+    System(x, dxdt);
+    for (size_t i = 0; i < dim; i++) {
+        x[i] = x[i] + dxdt[i] * dt;
+    }
 }
 
 
@@ -23,5 +28,13 @@ int main(){
     std::ofstream outfile;  //file handle to save the results in a file
     outfile.open("data for plotting.txt", std::ios::out | std::ios::trunc );
     
+    state_type x = {0, 0.01};
+    for (size_t i = 0; i < 1000; i++) {
+        euler_step(System, x, 0.01);
+        outfile << x[0] << "\t" << x[1] << "\n";
+    }
+    
+    
+
     outfile.close();
 }
