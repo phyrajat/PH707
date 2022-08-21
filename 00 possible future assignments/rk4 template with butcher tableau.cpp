ae59a43925f727a0acdc095ecced2b8bf5ed7559
Rajat Kumar Mandal <r.mandal@iitg.ac.in>
2022-08-21 19:03:56 +0530

    working but incorrect

--- 00 possible future assignments/rk4 template with butcher tableau.cpp
+++ 00 possible future assignments/rk4 template with butcher tableau.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <cmath>
 #include <array>
+#include <vector>
 #include <map>
 
 //Constant expressions appearing in the problem
@@ -10,8 +11,48 @@ constexpr double PI = 3.14159265359;    //value of PI
 constexpr double rollnum = 0.226121014;  //my roll number
 
 //Definition of data types in the problem
-typedef std::array<double, dimension> state_type;  //data type definition for dependant variables - array of x_0, x_1, ... x_n
-typedef std::map<double, state_type> solution;   //data type definition for storing the list of calculated values ((hash)map of time -> state)
+typedef std::array<double, dimension> state_type;   //data type definition for dependant variables - array of x_0, x_1, ... x_n
+typedef std::map<double, state_type> solution;  //data type definition for storing the list of calculated values ((hash)map of time -> state)
+
+//Class template for the Runge Kutta solver using Butcher tableau
+template <class State_Type, size_t order> class explicit_rk {
+    typedef std::array<double, order> butcher_coefficients;
+    typedef std::array<std::array<double, order>, order> butcher_matrix;
+private:
+    //information about the Butcher tableau
+    butcher_matrix a;
+    butcher_coefficients b, c;
+    //temporary variables for intermediate steps
+    std::array<State_Type, order> k;
+public:
+    //Constructor
+    explicit_rk(butcher_matrix A, std::array<double, order> B, std::array<double, order> C) : a(A), b(B), c(C) {
+
+    }
+
+    //Destructor
+    ~explicit_rk() {
+
+    }
+
+    //The stepper function, calculates x_{n+1} given the butcher tableau, differential equation, x_{n}, t and step size
+    void do_step(void (*Diff_Equation)(const State_Type& x, const double& t, State_Type& dxdt), State_Type& x, const double& t, const double& dt){
+        State_Type result = x;
+        for (size_t i = 0; i < order; i++) {
+            State_Type sum, dxdt;
+            for (size_t j = 0; j < i; j++) {
+                sum = sum + dt * a[i][j] * k[j];
+            }
+            sum = x + sum;
+            Diff_Equation(sum, t + c[i] * dt, dxdt);
+            k[i] = dxdt;
+        }
+        for (size_t i = 0; i < order; i++) {
+            result = result + dt * b[i] * k[i];
+        }
+        x = result;
+    }
+}; 
 
 //Overload the + operator to be able to add two vectors
 state_type operator + (state_type const &x, state_type const &y) {
@@ -37,22 +78,9 @@ void Pendulum(const state_type& x, const double& t, state_type& dxdt){
     dxdt[1] = -4.0 * PI * PI * sin(x[0]);
 }
 
-//The stepper function, iteratively calculates x_{n+1} given the differential equation, x_{n} and step size
-void rk4_step(void (*Diff_Equation)(const state_type& x, const double& t, state_type& dxdt), state_type& x, const double& t, const double& dt){
-    //temporary variables for intermediate steps
-    state_type k1, k2, k3, k4;
-
-    //calculate the intermediate values
-    Diff_Equation(x, t, k1);    //calculate k1
-    Diff_Equation(x + (dt / 2.0) * k1, t + dt / 2.0, k2);   //calculate k2
-    Diff_Equation(x + (dt / 2.0) * k2, t + dt / 2.0, k3);   //calculate k3
-    Diff_Equation(x + dt * k3, t + dt, k4); //calculate k4
-
-    //calculate x_{n+1} using the RK4 formula and return the results
-    x = x + (dt / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4);
-}
-
 int main(){
+    explicit_rk <state_type, 4> rk4_stepper({0,0,0,0,.5,0,0,0,0,.5,0,0,0,0,1,0}, {1.0/6.0 , 1.0/3.0 , 1.0/3.0 , 1.0/6.0}, { 0.0 , 0.5 , 0.5 , 1.0});    //Using the class template, creates a class object for the Runge Kutta solver with a given butcher tableau
+
     solution x_t;   //variable to store the calculations
 
     size_t STEPS = 1000;  //number of steps
@@ -64,7 +92,7 @@ int main(){
     //Step through the domain of the problem and store the solutions
     x_t[t_0] = x;   //store initial values
     for (size_t i = 0; i < STEPS; i++) {
-        rk4_step(Pendulum, x, NULL, dt);    //step forward
+        rk4_stepper.do_step(Pendulum, x, NULL, dt);    //step forward
         x_t[t_0 + i * dt] = x;  //store the calculation
     }
 
